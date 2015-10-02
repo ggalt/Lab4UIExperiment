cf472d21c441a85e669e1925dd8fc170a0ac46f8
George Galt <george@galtfamily.net>
2015-10-02 18:40:53 -0400

    Gradient Fixed

--- LabUIExperiment/app/src/main/java/coursera/lab/george/labuiexperiment/MainActivity.java
+++ LabUIExperiment/app/src/main/java/coursera/lab/george/labuiexperiment/MainActivity.java
@@ -1,9 +1,25 @@
 package coursera.lab.george.labuiexperiment;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.MainLayout);
+        DisplayMetrics metrics = getBaseContext().getResources().getDisplayMetrics();
+
+        GradientView gv = new GradientView(this, metrics.widthPixels, metrics.heightPixels);
+        relativeLayout.addView(gv);
     }
 
     @Override
@@ -33,5 +54,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class GradientView extends View {
+
+        private Paint mPaint;
+        private float centerX;
+        private float centerY;
+        private int width;
+        private int height;
+        private int[] gColors = new int[]{Color.BLUE, Color.YELLOW, Color.RED, Color.GREEN};
+        private boolean mSetShader = false;
+        Bitmap bitmap;
+
+        OnTouchListener touchListener;
+
+        public GradientView(Context c, int w, int h) {
+            super(c);
+            mPaint = new Paint();
+            mPaint.setAntiAlias(false);
+            mPaint.setStyle(Paint.Style.FILL);
+            width = w;
+            height = h;
+            bitmap  = Bitmap.createBitmap(width,height, Bitmap.Config.ARGB_8888);
+            setClickable(true);
+            centerX = (float) getWidth()/2F;
+            centerY = (float) getHeight()/2F;
+            touchListener = new OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    centerX = event.getX();
+                    centerY = event.getY();
+                    invalidate();
+                    return false;
+                }
+            };
+            setOnTouchListener(touchListener);
+        }
+
+        @Override
+        public void setOnTouchListener(OnTouchListener l) {
+            super.setOnTouchListener(l);
+        }
+
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            if(!mSetShader) {
+                RadialGradient grad = new RadialGradient(centerX, centerY, (float)canvas.getHeight(), gColors, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(grad);
+            }
+            canvas.drawPaint(mPaint);
+            invalidate();
+        }
+
     }
 }
